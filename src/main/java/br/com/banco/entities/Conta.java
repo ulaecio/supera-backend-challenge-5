150c5ad85c23dc73b83c2fe8d4c247d40263b5a8
ulaecio <ulaecio@live.com>
2023-07-16 22:58:30 -0300

    Project created with Entities

---
+++ src/main/java/br/com/banco/entities/Conta.java
@@ -0,0 +1,34 @@
+package br.com.banco.entities;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "conta")
+public class Conta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_conta")
+    private Long id;
+
+    @Column(name = "nome_responsavel", nullable = false)
+    private String nomeResponsavel;
+
+    // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNomeResponsavel() {
+        return nomeResponsavel;
+    }
+
+    public void setNomeResponsavel(String nomeResponsavel) {
+        this.nomeResponsavel = nomeResponsavel;
+    }
+}
